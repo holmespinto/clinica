2bcdd38fdbb9d40adaa8c9456cd11aa9e7f62bdf
hepinto <holmespinto@unicesar.edu.co>
2021-06-29 09:03:26 -0500

    cambios en el comentario

--- farmacia/compras.php
+++ farmacia/compras.php
@@ -10,6 +10,7 @@
  * @copyright Copyright (c) 2014 Rod Roark <rod@sunsetsystems.com>
  * @copyright Copyright (c) 2018 Brady Miller <brady.g.miller@gmail.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ * DESARRLLADO POR HOLMES
  */
 
 set_time_limit(0);
